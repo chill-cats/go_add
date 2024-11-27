032330dfe512500d3d10b867d3e0aa102c1fced3
Hoàng Nhân Lê Nguyễn <nhanlenguyen@Nhan-Air.local>
2024-11-27 14:36:38 +0700

    Version v2.0.0

--- add.go
+++ add.go
@@ -1,8 +1,14 @@
 package go_add
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add This function sum two number together. See [Addition]
 //
 // [Addition]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/chill-cats/go_add
 
 go 1.23.3
+
+require golang.org/x/exp v0.0.0-20241108190413-2d47ceb2692f
