4b0b8ea038800a1c293b652c1d877fb7f8d5d079
Hoàng Nhân Lê Nguyễn <nhanlenguyen@Nhan-Air.local>
2024-11-27 14:34:07 +0700

    Version v1.0.1

--- add.go
+++ add.go
@@ -1,5 +1,8 @@
 package go_add
 
+// Add This function sum two number together. See [Addition]
+//
+// [Addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
